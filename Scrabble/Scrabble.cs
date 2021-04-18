ab0d93ab54073e63a8083e885fc571505d007093
cisbell5 <47825787+cisbell5@users.noreply.github.com>
2021-04-18 16:41:51 -0500

    fixed assembly issues

--- Scrabble/Scrabble.cs
+++ Scrabble/Scrabble.cs
@@ -5,36 +5,63 @@ namespace Scrabble
 {
     class ScrabbleMain 
     {
+        static Dictionary<char,int> LetterValLUT;
+
         static void Main(string[] args) 
         {
-            //TODO: use thread for this
-            System.IO.StreamReader dict = new System.IO.StreamReader("dictionary.txt");
+            LetterValLUT = new Dictionary<char,int>()
+            {
+                {'A', 1}, {'B', 3}, {'C', 3}, {'D', 2}, {'E', 1}, {'F', 4},
+                {'G', 2}, {'H', 4}, {'I', 1}, {'J', 8}, {'K', 5}, {'L', 1},
+                {'M', 3}, {'N', 1}, {'O', 1}, {'P', 3}, {'Q', 10}, {'R', 1},
+                {'S', 1}, {'T', 1}, {'U', 1}, {'V', 4}, {'W', 4}, {'X', 8},
+                {'Y', 4}, {'Z', 10}
+            };
             while (true) {
-                string letters = Console.ReadLine();  
-                if (letters.Length > 8 || letters.Length < 1) {
-                    Console.WriteLine("INVALID CHARACTER AMOUNT");
-                    continue;
-                }
-
-                List<string> perms = Permutations.Start(letters);
-                List<string> match = new List<string>();
-                
-                //Reading from dictionary
-                string word; 
-                while ((word = dict.ReadLine()) != null) {
-                    foreach (string s in perms) {
-                        int len = (word.Length > s.Length) ? s.Length : word.Length;
-                        string comp = s.Substring(0, len);
-                        if (String.Equals(comp, word)) {
-                            Console.WriteLine(word);
-                            match.Add(word);
-                            Console.WriteLine(match.Count);
-                        }
-                    }
-                }    
-                break;     
+                string letters = Console.ReadLine();
+                string word = GetHighestValue(letters);
             }
-        }        
+        }
+        
+        static string GetHighestValue(string letters) 
+        {
+            //TODO: use thread for this
+            System.IO.StreamReader dict = new System.IO.StreamReader("Scrabble/dictionary.txt");
+            
+            //max value, string corresponding to it
+            Tuple<string, int> max = new Tuple<string, int>("", 0);
+ 
+            if (letters.Length > 8 || letters.Length < 1) {
+                Console.WriteLine("INVALID CHARACTER AMOUNT");
+                return null;
+            }
+
+            List<string> perms = Permutations.Start(letters);
+            List<string> match = new List<string>();
+            
+            //Reading from dictionary
+            string word; 
+            while ((word = dict.ReadLine()) != null) {
+                foreach (string s in perms) {
+                    int len = (word.Length > s.Length) ? s.Length : word.Length;
+                    string comp = s.Substring(0, len);
+                    if (String.Equals(comp, word)) {
+                        match.Add(word);
+                    }
+                }
+            }
+
+            foreach(string s in match) {
+                int sum = 0; 
+                foreach(char c in s) sum += LetterValLUT[c];
+                if (sum > max.Item2) max = new Tuple<string, int>(s, sum);
+
+                Console.WriteLine("possible {0}", s);
+            }
+            Console.WriteLine("str {0} val {1}", max.Item1, max.Item2);
+
+            return max.Item1;
+        }
     }
 }
 
